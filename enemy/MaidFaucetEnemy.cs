abe5e9352bdef7e906ea177e1963088425239a1f
SHIRAISHI <SHIRAISHI@SH76K>
2024-11-01 10:50:13 +0900

    ロード再開時にプレーヤーが少し落下する メイドの出る蛇の強化

--- enemy/MaidFaucetEnemy.cs
+++ enemy/MaidFaucetEnemy.cs
@@ -1,5 +1,6 @@
 using Godot;
 using tmfos.mob;
+using static tmfos.enemy.EnemySpawner;
 
 namespace tmfos.enemy;
 
@@ -37,12 +38,17 @@ public partial class MaidFaucetEnemy : PathFollowEnemy
             return;
         }
 
+        RandomNumberGenerator random = new();
+        EnemyResourceName enemyName = random.RandiRange(0, 15) == 0 ? EnemyResourceName.RocketMaidEnemy : EnemyResourceName.NekomimiMaidEnemy;
+
         if (m_direction is DirectionType.Left or DirectionType.Up)
         {
+            _spawnerLeft.EnemyName = enemyName;
             _spawnerLeft.SpawnEnemy();
         }
         else if (m_direction is DirectionType.Right or DirectionType.Down)
         {
+            _spawnerRight.EnemyName = enemyName;
             _spawnerRight.SpawnEnemy();
         }
     }
