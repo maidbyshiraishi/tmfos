a876ac2fe2bae12097bd2ef42354f0b812d1ed82
SHIRAISHI <SHIRAISHI@SH76K>
2026-01-19 14:26:04 +0900

    ボタンを非表示から無効に変更 不要な処理を削除

--- screen/SaveAndLoadDialog.cs
+++ screen/SaveAndLoadDialog.cs
@@ -18,31 +18,21 @@ public partial class SaveAndLoadDialog : DialogRoot
     private void UpdateTimeStamp()
     {
         GameData gdata = GetNode<GameData>("/root/GameData");
-        string[] date = gdata.GetFileDates();
-
-        for (int i = 1; i <= GameData.NumOfSaveFiles; i++)
-        {
-            if (date[i - 1] is not null)
-            {
-                GetNode<Control>("Control/Load").Show();
-                break;
-            }
-        }
 
         if (gdata.HasLastSlotNo())
         {
             int lastSlotNo = gdata.LastSlotNo;
-            GetNode<Control>("Control/LastLoad").Show();
+            GetNode<BaseButton>("Control/LastLoad").Disabled = false;
             OpenLoadConfirmDialogCommand load = GetNode<OpenLoadConfirmDialogCommand>("Control/LastLoad/Exec/OpenLoadConfirmDialogCommand");
             load.SlotNo = lastSlotNo;
             OpenSaveConfirmDialogCommand save = GetNode<OpenSaveConfirmDialogCommand>("Control/LastSave/Exec/OpenSaveConfirmDialogCommand");
             save.SlotNo = lastSlotNo;
-            GetNode<Control>("Control/LastSave").Show();
+            GetNode<BaseButton>("Control/LastSave").Disabled = false;
         }
         else
         {
-            GetNode<Control>("Control/LastLoad").Hide();
-            GetNode<Control>("Control/LastSave").Hide();
+            GetNode<BaseButton>("Control/LastLoad").Disabled = true;
+            GetNode<BaseButton>("Control/LastSave").Disabled = true;
         }
     }
 
